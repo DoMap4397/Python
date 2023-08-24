af345734f464d3a0efa83a9969cb78abd7dcdf75
DoMap <73330621+DoMap4397@users.noreply.github.com>
2023-08-25 01:18:49 +0700

    Add files via upload

--- Flask Python/alchemy.py
+++ Flask Python/alchemy.py
@@ -1,16 +1,28 @@
 from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
+from os import path
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Test'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.permanent_session_lifetime = timedelta(seconds=10)
 
-
-@app.route('/')
-def home():
-    return render_template('home.html')
+db = SQLAlchemy(app)
 
 
+class User(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+
+@app.route('/home')
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
@@ -19,16 +31,16 @@ def login():
         if user_name:
             session['user'] = user_name
             flash('Đã đăng nhập thành công!', 'info')
-            return redirect(url_for('hello_user', name=user_name))
+            return redirect(url_for('user', name=user_name))
     if 'user' in session:
         name = session['user']
         flash('Đăng nhập không thành công!', 'info')
-        return render_template('user.html', user=name)
+        return redirect(url_for('user', name=user_name))
     return render_template('login.html')
 
 
 @app.route('/user')
-def hello_user():
+def user():
     if 'user' in session:
         name = session['user']
         return render_template('user.html', user=name)
@@ -45,4 +57,7 @@ def log_out():
 
 
 if __name__ == '__main__':
+    if not path.exists('user.db'):
+        db.create_all(app = app)
+        print('Created database')
     app.run(debug=True)
